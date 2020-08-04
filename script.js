9597ecdd77e2f309bd9384108bde49e7dea4236f
Asmit Nayak <asmit.nayak@gmail.com>
2020-08-03 23:42:21 -0500

    Added name and icons

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-$(document).ready(function () {
-    $(".fa").each(function () {
-        var randLeft = Math.floor(Math.random() * 90) + 1;
-        var randfontSize = Math.floor(Math.random() * 3) + 8;
-        var randRotation = Math.floor(Math.random() * 360) + 1;
-        var randDelay = Math.floor(Math.random() * 5) + 1;
-        var randDuration = Math.floor(Math.random() * 10) + 5;
+$(function () {
+    $(".icm").each(function () {
+        let randLeft = Math.floor(Math.random() * 90) + 1;
+        let randfontSize = Math.floor(Math.random() * 3) + 8;
+        let randRotation = Math.floor(Math.random() * 360) + 1;
+        let randDelay = Math.floor(Math.random() * 5) + 1;
+        let randDuration = Math.floor(Math.random() * 10) + 5;
         $(this).attr("style", "left:" + randLeft + "vw; font-size:" + randfontSize + "vw; transform: rotate(" + randRotation + "deg); animation-delay: " + randDelay + "s; animation-duration: " + randDuration + "s;)");
     });
 });
