62fd51920c398744f743b822019c7909160ae9a9
Asmit Nayak <asmit.nayak@gmail.com>
2020-08-04 15:44:38 -0500

    Added some animations

--- script_down.js
+++ script_down.js
@@ -5,14 +5,14 @@ function myFunction(x) {
     x.classList.toggle("change");
     if (times % 2 === 1){
         document.getElementById("mySidenav").style.width = "250px";
-        // document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+        document.getElementById("overlay").style.backgroundColor= "rgba(0,0,0,0.4)";
         // let bars = document.getElementsByClassName("ham-bar")[0];
         document.getElementsByClassName("bar1")[0].style.backgroundColor = "white";
         document.getElementsByClassName("bar2")[0].style.backgroundColor = "white";
         document.getElementsByClassName("bar3")[0].style.backgroundColor = "white";
     } else {
         document.getElementById("mySidenav").style.width = "0px";
-        // document.body.style.backgroundColor = "white";
+        document.getElementById("overlay").style.backgroundColor= "transparent";
         document.getElementsByClassName("bar1")[0].style.backgroundColor = "black";
         document.getElementsByClassName("bar2")[0].style.backgroundColor = "black";
         document.getElementsByClassName("bar3")[0].style.backgroundColor = "black";
