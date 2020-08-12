48b3ee7a2ffcc433a963d8cf14e62204cfdd9503
Asmit <asmit.nayak@gmail.com>
2020-08-12 02:21:52 -0500

    Minor JS tweaks

--- script_down.js
+++ script_down.js
@@ -1,23 +1,30 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 let times = 1;
 
-function myFunction(x) {
+function myFunction() {
+    let x = document.getElementsByClassName("ham-bar")[0];
     x.classList.toggle("change");
-    if (times % 2 === 1){
+    if (times % 2 === 1) {   // entry
         document.getElementById("mySidenav").style.width = "250px";
-        document.getElementById("overlay").style.backgroundColor= "rgba(0,0,0,0.4)";
+        document.getElementById("overlay").style.backgroundColor = "rgba(0,0,0,0.4)";
         // let bars = document.getElementsByClassName("ham-bar")[0];
         document.getElementsByClassName("bar1")[0].style.backgroundColor = "white";
         document.getElementsByClassName("bar2")[0].style.backgroundColor = "white";
         document.getElementsByClassName("bar3")[0].style.backgroundColor = "white";
         document.getElementById("down_arrow").style.zIndex = "49";
+        document.getElementsByClassName("carousel_buttons")[0].style.zIndex = "49";
+        document.getElementsByClassName("carousel_buttons")[1].style.zIndex = "49";
+        document.getElementsByClassName("carousel_buttons")[2].style.zIndex = "49";
     } else {
         document.getElementById("mySidenav").style.width = "0px";
-        document.getElementById("overlay").style.backgroundColor= "transparent";
+        document.getElementById("overlay").style.backgroundColor = "transparent";
         document.getElementsByClassName("bar1")[0].style.backgroundColor = "black";
         document.getElementsByClassName("bar2")[0].style.backgroundColor = "black";
         document.getElementsByClassName("bar3")[0].style.backgroundColor = "black";
-        document.getElementById("down_arrow").style.zIndex = "51";
+        document.getElementById("down_arrow").style.zIndex = "52";
+        document.getElementsByClassName("carousel_buttons")[0].style.zIndex = "52";
+        document.getElementsByClassName("carousel_buttons")[1].style.zIndex = "52";
+        document.getElementsByClassName("carousel_buttons")[2].style.zIndex = "52";
     }
     times += 1;
 }
